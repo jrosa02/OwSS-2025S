9b5348de1b6822ac8fe635216650828b33a5191e
Jan Rosa <jrosa@student.agh.edu.pl>
2025-05-19 09:15:46 +0200

    first working programm XD

--- cost_fun_s.m
+++ cost_fun_s.m
@@ -3,9 +3,9 @@ function [q,g]=cost_fun_s(u,tau,x0,xf,W,MDNS)
 %calkowanie rownania stanu
 [~,x]=get_tx_s(tau,u,x0,MDNS);
 % roznica pomiedzy stanem koncowym i docelowym
-dxend=(x(end,1:end)-xf')';
+dxend=(x(end,1:end-1)-xf')';
 % funkcja celu
-q= tau(end) + dxend' * W * dxend + x(end, 7);
+q= tau(end) + dxend' * W * dxend + x(end, 6);
 if nargout>1
 %opcjonalne obliczenie gradientu
 %g=get_grad_s(tau,u,x0,xf,W,MDNS);
--- printOrbit.m
+++ printOrbit.m
@@ -1,42 +1,53 @@
 function [] = printOrbit(xout, yout, zout)
 %PRINTORBIT Wyświetla orbitę wokół Ziemi z układem współrzędnych ECEF
 % xout, yout, zout - współrzędne orbity (w km)
-R = 6.37314*1e6 ;
 
-% Krok 1: Parametry Ziemi
-earth_texture = imread('zdjecia\earthsTexture.jpg'); % Plik graficzny tekstury Ziemi
+R = 6.37314e6;            % Promień Ziemi w metrach
+R0 = 4.2242e7;            % Promień koła w metrach
 
+% Wczytanie tekstury Ziemi
+earth_texture = imread('zdjecia\earthsTexture.jpg');
+
+% Przygotowanie sfery Ziemi
 figHandle = figure(); hold on;
 figHandle.Position = [100, 100, 800, 600];
+
 [X, Y, Z] = sphere(100);
-X = X * R; % Promień Ziemi w km
+X = X * R;
 Y = Y * R;
-Z = -Z * R; % Odwrócenie osi Z, aby pasowało do mapy
+Z = -Z * R;
 
-surf(X, Y, Z, 'CData', earth_texture, 'FaceColor', 'texturemap', 'EdgeColor', 'none')
+% Rysowanie Ziemi z teksturą
+surf(X, Y, Z, 'CData', earth_texture, 'FaceColor', 'texturemap', 'EdgeColor', 'none');
 
-plot3(xout, yout, zout, 'c-', 'LineWidth', 2); % Trajektoria orbity w kolorze czerwonym
+% Rysowanie trajektorii orbity
+plot3(xout, yout, zout, 'c-', 'LineWidth', 2);
 
-axis_length = 2*R; % Długość osi względem promienia Ziemi
+% Rysowanie osi ECEF
+axis_length = 2 * R;
 
-% Oś X (w kolorze czerwonym)
 quiver3(0, 0, 0, axis_length, 0, 0, 'r', 'LineWidth', 2, 'MaxHeadSize', 0.5);
 text(axis_length, 0, 0, 'X', 'FontSize', 12, 'Color', 'r');
 
-% Oś Y (w kolorze zielonym)
 quiver3(0, 0, 0, 0, axis_length, 0, 'g', 'LineWidth', 2, 'MaxHeadSize', 0.5);
 text(0, axis_length, 0, 'Y', 'FontSize', 12, 'Color', 'g');
 
-% Oś Z (w kolorze niebieskim)
 quiver3(0, 0, 0, 0, 0, axis_length, 'b', 'LineWidth', 2, 'MaxHeadSize', 0.5);
 text(0, 0, axis_length, 'Z', 'FontSize', 12, 'Color', 'b');
 
-view([109 32]); xlabel("X"); ylabel("Y"); zlabel("Z");
+% === Dodanie koła o promieniu R0 w płaszczyźnie XY ===
+theta = linspace(0, 2*pi, 500);
+circle_x = R0 * cos(theta);
+circle_y = R0 * sin(theta);
+circle_z = zeros(size(theta)); % na płaszczyźnie Z=0
+
+plot3(circle_x, circle_y, circle_z, 'r--', 'LineWidth', 2);
+
+% Ustawienia widoku
+view([109 32]);
+xlabel("X"); ylabel("Y"); zlabel("Z");
 axis equal;
 grid on;
 
-%testNEDField([xout, yout, zout]);
-
 hold off;
 end
-
